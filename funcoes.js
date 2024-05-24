43e3fb97bd6f738ddd443152cb26450a4e77a88a
Aline <aline.cordeirochaves@gmail.com>
2024-05-24 13:55:57 -0300

    Alterar arquivos

--- funcoes.js
+++
@@ -1,6 +0,0 @@
-// funcoes.js
-function calcularPrecoTotal(itens) {
-    return itens.reduce((total, item) => total + item.preco, 0);
-}
-
-module.exports = calcularPrecoTotal;
--- jest.config.js
+++
@@ -1,6 +0,0 @@
-// jest.config.js
-module.exports = {
-    testMatch: ['**/*.test.js'], // padrão para encontrar arquivos de teste
-    // outras configurações...
-  };
-  
